5f584d6cb7e56d367c2a557c39e8b79474432b45
bjarkt <bjarke.tobiesen@gmail.com>
2021-07-18 11:56:17 +0200

    typo fix

--- deployment.sh
+++ deployment.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker-compose build && docker-compose push
-docker stack deploy --compose=docker-compose.yml --with-registry-auth timetracker
+docker stack deploy --compose-file=docker-compose.yml --with-registry-auth timetracker
